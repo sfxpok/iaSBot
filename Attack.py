eeff064e8ed8bdb892ea2efb911db98683192525
Pedro Rodrigues <pedroagrodrigues@gmail.com>
2018-12-15 11:54:17 +0000

    Attack is finished
    Small fixes on the Attack file

--- Attack.py
+++ Attack.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python3
 from ev3dev2.motor import OUTPUT_A, OUTPUT_B, OUTPUT_D, MoveTank
 from Motor import MediumMotor
-from ev3dev2.sound import Sound
-import time
-
 
 def punch():
     engine = MediumMotor(OUTPUT_D, 100)
     mov = MoveTank(OUTPUT_A, OUTPUT_B)
     print('Punching!')
-    time.sleep(1)
     mov.on_for_degrees(30,-30, 100)    
-    time.sleep(0.001)
     engine.movementDeg(-362)
     engine.movementDeg(-362)
     mov.on_for_degrees(-30, 30, 100)
-    time.sleep(0.001)
 
 
 def shoot():
