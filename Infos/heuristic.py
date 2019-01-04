4fe8269a82574b3bbd11ac5a4f874c6be1386d47
Pedro Rodrigues <pedroagrodrigues@gmail.com>
2019-01-04 16:15:58 +0000

    Heuristic development
    Added functions for heuristic

--- Infos/heuristic.py
+++ Infos/heuristic.py
@@ -15,110 +15,117 @@ heurValueMap = [[30,6,6,6,6,30],
                 [4,2,2,2,2,4],
                 [30,6,6,6,6,0]]
 
+smellValues = {
+    'Green': 0,
+    'Yellow': danger1Value,
+    'Red': danger2Value,
+    'Blue': danger3Value,
+    'Brown': danger4Value
+}
+zombieValues = {
+    'Green': 0,
+    'Yellow': ZombieValue,
+    'Red': ZombieValue,
+}
+pieceValues = {
+    'Green': 0,
+    'Yellow': pieceValue,
+    'Red': bulletValue
+}
+
 bulletValue = -10
 pieceValue = -15
 checkedHouseValue = 1
-ZombieValue = 20
+ZombieValue = 40
 danger1Value = 1
 danger2Value = 2
 danger3Value = 3
 danger4Value = 4
 
+lastTurnSmell = 0
 
 def listActions():
-    if perigo == 0:
+    if lastTurnSmell == 0:
         search()
         scout()
-    if perigo == 1:
-        search()
+    else:
         scout()
-        if nextToZombie():
-            attack(zombie_position) 
-    if perigo == 2:
-        scout()
-        if nextToZombie():
-            attack(zombie_position)
-            search()
-    if perigo == 3:
-        scout()
-        attack(zombie_position)
-        search()
-    if perigo == 4:
-        scout()
-        attack(zombie_position)
+        zombiesDirections = whereZombie()
+        if zombiesDirections[0] != [] or zombiesDirections[1] != []:
+            attack(zombiesDirections)
         search()
 
-def attack(zombie_position):
-    setDirection(zombie_position)
-    if bullet:
+def attack(zombiesDirections):
+    if zombiesDirections[0] != []:
+        setDirection(zombiesDirections[0][0])
+    else:
+        setDirection(zombiesDirections[1])
+    if hasBullet == True:
         shoot()
     else:
         punch()
 
-def nextToZombie():
-    return (existZombie(curentX+1,currentY+1) or
-        existZombie(curentX+1,currentY-1) or
-        existZombie(curentX-1,currentY+1) or
-        existZombie(curentX-1,currentY-1))
 
-def existZombie(targetX, targetY):
+def whereZombie(itemsAround=0):
+    #Esta funcao recebe o array de items da funcao reconhecimento
+
+    directions = {
+        0: 'North',
+        1: 'East',
+        2: 'South',
+        3: 'West'
+    }
+
+    arrayZombiesDirections = [[],[]]
+
+    for i in range(4):
+        direction = directions[i]
+        zombieColor = itemsAround[direction][1]
+        if zombieColor == 'Yellow':
+            arrayZombiesDirections[0].append(direction)
+        elif zombieColor == 'Red':
+            arrayZombiesDirections[1].append(direction)
+    
+    return arrayZombiesDirections
+
+
+def saveSmell(colorsValues):
+    #colorsValues[0]-->SmellNorth
+    #colorsValues[1]-->SmellEast
+    #colorsValues[2]-->SmellSouth
+    #colorsValues[3]-->SmellWest
+
     
 
 def addItemsToValues(itemsAround=None):
-    # bulletValue = -10
-    # pieceValue = -15
-    # checkedHouseValue = 1 Acrescentar depois de movimento
-    # ZombieValue = 40
-    # danger1Value = 1
-    # danger2Value = 2
-    # danger3Value = 3
-    # danger4Value = 4
-
-    #Colors:
-    #Cheiro: Vermelho, Amarelo, Azul, Castanho
-    #Zombie: Vermelho, Castanho
-    #Peça: Azul, Verde
 
     if itemsAround == None:
         return [0,0,0]
 
-    smellValues = {
-        'White': 0,
-        'Red': 1,
-        'Yellow': 2,
-        'Blue': 3,
-        'Brown': 4
-    }
-    zombieValues = {
-        'White': 0,
-        'Red': 40,
-        'Brown': 40,
-    }
-    pieceValues = {
-        'White': 0,
-        'Blue': -15,
-        'Green': -10
-    }
-
     counter = 1
     addValues = []
+    colorsValues = []
 
     for direction in itemsAround:
         for color in direction:
-            colorsValues = []
+            directionValue = 0
             if counter == 1:
-                colorsValues.append(smellValues[color])
+                colorValue = smellValues[color]
+                directionValue += colorValue
+                colorsValues.append(colorValue)
             elif counter == 2:
-                colorsValues.append(zombieValues[color])
+                directionValue += zombieValues[color]
             elif counter == 3:
-                colorsValues.append(pieceValues[color])
+                directionValue += zombieValues[color]
         counter += 1
-        addValues.append(colorsValues)
+        addValues.append(directionValue)
+
+    lastTurnSmell = saveSmell(colorsValues)
     
     return addValues
     
 
-def nextHouse(itemsAround=None):
+def bestNextHouse(itemsAround=None):
     addValues = addItemsToValues(itemsAround)
 
     north = realValuesMap[currentX][currentY-1] + heurValueMap[currentX][currentY-1]+addValues[0]
@@ -141,5 +148,5 @@ def nextHouse(itemsAround=None):
     return targetHouse
 
 def search():
-    targetHouse = nextHouse() #Inserir nesta funçao o array das cores
+    targetHouse = bestNextHouse() #Inserir nesta funçao o array das cores
     move(targetHouse)
